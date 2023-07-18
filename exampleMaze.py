0e52de28aa5b6ab8867180e1b751c227996e8b91
goocy <gooocy@gmail.com>
2023-07-18 23:40:47 +0200

    Updated the import procedure to Python 3. Ahem.

--- exampleMaze.py
+++ exampleMaze.py
@@ -1,16 +1,16 @@
 import maze.helpers
-import maze.excavation
 import maze.plotTools
+import maze.excavation
 
 # Generate a maze
 roomSize = (13,25)
 flatness = (0.5,-0.5)
 room = maze.helpers.Room(roomSize)
 print(room.dimensions)
-#holeyRoom = maze.excavation.createHoles(room, sum(roomSize) * 0.05, 5) # 5 holes with a total volume of 5%
+holeyRoom = maze.excavation.createHoles(room, sum(roomSize) * 0.05, 5) # 5 holes with a total volume of 5%
 entrancePoint = (0,11)
 print('Carving passages...')
-exampleMaze, stackSize = maze.excavation.carvePassages(room, entrancePoint, flatness, exitWallSide = exitWallSide, livePlot=False)
+exampleMaze, stackSize = maze.excavation.carvePassages(holeyRoom, entrancePoint, flatness, livePlot=True)
 
 # Render the maze to voxels
 passageSize = 8
--- maze/__init__.py
+++
@@ -1,4 +0,0 @@
-from excavation import *
-from helpers import *
-from plotTools import *
-from minecraft import *
